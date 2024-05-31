935671016353ecae454186da936c578630622017
antoine-prieur1 <antoine.prieur@celliersinteligents.com>
2024-05-30 22:31:52 -0400

    update git plugins shortcuts

--- .config/nvim/lua/custom/plugins/gitsigns.lua
+++ .config/nvim/lua/custom/plugins/gitsigns.lua
@@ -57,14 +57,15 @@ return {
       map('n', '<leader>gb', function()
         gs.blame_line { full = false }
       end, { desc = 'git blame line' })
-      map('n', '<leader>gd', gs.diffthis, { desc = 'git diff against index' })
+      map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
       map('n', '<leader>hD', function()
         gs.diffthis '~'
       end, { desc = 'git diff against last commit' })
 
       -- Toggles
       map('n', '<leader>hB', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-      map('n', '<leader>hd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+
+      map('n', '<leader>gd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
 
       -- Text object
       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
