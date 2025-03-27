da72cbd1f7dcd83c6f97c129e772ac07130f8912
Antoine Prieur <antoine.prieur45@gmail.com>
2025-03-27 05:56:13 -0400

    fix: update snack config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -8,11 +8,11 @@ return {
     -- refer to the configuration section below
     -- bigfile = { enabled = true },
     dashboard = { enabled = true },
-    -- explorer = { enabled = true },
+    -- explorer = { enabled = true, hidden = true, git_ignored = true, respect_gitignore = false, allow_patterns = { ".*" } },
     image = { enabled = true },
     -- indent = { enabled = true },
     -- input = { enabled = true },
-    picker = { enabled = true },
+    picker = { enabled = true, ignored = true, git_ignored = true, exclude = { "**/.git/*" } },
     -- notifier = { enabled = true },
     -- quickfile = { enabled = true },
     -- scope = { enabled = true },
